52e8757664e7505becfc6958238f0cc22b927e42
Luis Cabandez <mrcabandez@gmail.com>
2022-12-13 12:10:46 +0800

    Add files via upload

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -17,14 +17,28 @@ const GenerateQuote = async () => {
  function Bible(){
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
-    document.getElementById("bible-course").innerHTML = this.responseText;
-        
-    }
+    let y = JSON.parse(this.responseText);    
+   
+      let MyHeaders = {
+        "Accept" : "application/json" 
+      } ;  
+     let htmlString ='';
+     for( let Item in y ) {
+        htmlString += ` ${Item}: ${y[Item]}`;
+     }
+        document.getElementById("bible-course").innerHTML =   htmlString ;
+        console.log(htmlString);
+}
+    
     var apiKey ="190c221dd7mshef713eb6b298096p1507d9jsn5d7fb4966a54";
     var BibleBook  = document.getElementById("Book").value;
     var Chapter = document.getElementById("Chapter").value;
     var Verse =document.getElementById("Verse").value;
     const url = xhttp.open("GET", `https://ajith-holy-bible.p.rapidapi.com/GetVerseOfaChapter?Book=${BibleBook}&chapter=${Chapter}&Verse=${Verse}&rapidapi-key=${apiKey}`);
     xhttp.send();
+
+
+    
+    
 
 }
